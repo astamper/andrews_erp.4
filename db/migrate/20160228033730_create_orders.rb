66d0a614fb2b57fba543310be9e0af1c51ba191d
Prelang Builder <builder@prelang.com>
2016-02-28 03:37:30 +0000

    Generated model Order with migration

---
+++ db/migrate/20160228033730_create_orders.rb
@@ -0,0 +1,22 @@
+class CreateOrders < ActiveRecord::Migration
+  def change
+    create_table :orders do |t|
+      t.string :status
+      t.string :first_name
+      t.string :last_name
+      t.string :address_1
+      t.string :address_2
+      t.string :city
+      t.string :state
+      t.string :zip
+      t.string :email
+      t.string :phone
+      t.date :fob_date
+      t.time :fob_time
+      t.references :customer, index: true
+      t.integer :customer_id
+
+      t.timestamps
+    end
+  end
+end
